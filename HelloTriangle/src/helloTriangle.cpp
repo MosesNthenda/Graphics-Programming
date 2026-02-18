1bac8d7678f846c40d218a1c98e9f0985dd333d0
MosesNthenda <bsc-com-18-23@unima.ac.mw>
2026-02-18 19:25:32 +0100

    Drawing a triangle, triangle not drawn yet

--- HelloTriangle/src/helloTriangle.cpp
+++ HelloTriangle/src/helloTriangle.cpp
@@ -23,10 +23,8 @@ const float VertexPositions[] = {
 
 void initializeVertexBuffer() {
 
-    glGenBuffers(1, &positionBufferObject);
     glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
     glBufferData(GL_ARRAY_BUFFER, sizeof(VertexPositions), VertexPositions, GL_STATIC_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 const char *chVertexShader = "#version 330\n"
@@ -81,11 +79,6 @@ GLuint CreateProgram(const std::vector<GLuint> &shaderList) {
     return shaderProgram;
 }
 
-void display() {
-
-    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(0);
-}
 
 int main() {
 
@@ -114,22 +107,6 @@ int main() {
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    while(!glfwWindowShouldClose(window)) {
-        
-        //input
-        processInput(window);
-
-        //rendering commands here
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        //check and call events and swap the buffers
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    initializeVertexBuffer();
-
     unsigned int vshader1;
     unsigned int fshader1;
 
@@ -142,11 +119,37 @@ int main() {
 
     theProgram = CreateProgram(shaderList);
 
-    glUseProgram(theProgram);
-
     for(unsigned int shader : shaderList) {
         glDeleteShader(shader);
     }
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    initializeVertexBuffer();
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0); 
+
+    while(!glfwWindowShouldClose(window)) {
+        
+        //input
+        processInput(window);
+
+        //rendering commands here
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(theProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        //check and call events and swap the buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 
     return 0;
 }
