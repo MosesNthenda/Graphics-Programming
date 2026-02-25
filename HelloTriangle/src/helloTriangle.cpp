a0dd40a8994d86289d47611b88fadd8d584c05e6
MOSES NTHENDA <BSC/COM/18/23@COMLAB.COM>
2026-02-25 16:09:06 +0100

    some modifications and an exercise

--- HelloTriangle/src/helloTriangle.cpp
+++ HelloTriangle/src/helloTriangle.cpp
@@ -153,7 +153,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "ColorTriangle", NULL, NULL);
     if( window == NULL) {
         
         std::cout << "Failed to create GLFW window" << std::endl;
